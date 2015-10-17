4ee37213c9365d6ee737914fc4ef1e090c820695
liuxiangwin <liuxiangwin@163.com>
2015-10-17 12:21:53 +0800

    Update the Author

--- Teli-source/ECP/ECP/custom/testrite/testritecore/src/com/testritegroup/ec/core/category/converter/poplator/ManafacturePopulator.java
+++ Teli-source/ECP/ECP/custom/testrite/testritecore/src/com/testritegroup/ec/core/category/converter/poplator/ManafacturePopulator.java
@@ -9,7 +9,13 @@ import com.testritegroup.ec.core.category.ManafactureData;
 import com.testritegroup.ec.core.model.ManafactureModel;
 
 
-
+/**
+ *
+ * @author liuxiangwin
+ *
+ * @param <SOURCE>
+ * @param <TARGET>
+ */
 public class ManafacturePopulator<SOURCE extends ManafactureModel, TARGET extends ManafactureData> implements
 		Populator<ManafactureModel, ManafactureData>
 //extends ProductPopulator
